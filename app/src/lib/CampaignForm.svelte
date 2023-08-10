b73d3f5b9c7398065ce7b2cf15e098a65d7ec01e
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-08-10 00:02:23 +0000

    Do logic in model

--- app/src/lib/CampaignForm.svelte
+++ app/src/lib/CampaignForm.svelte
@@ -19,12 +19,16 @@
 	let submitting = false;
 	let professionals = [];
 	let selectedCampaignTypeLabel = '';
-	let myData = { campaign_type: null }; // Or some default state
+	let overlayVisible = true;
 
 	const campaignTypes = [
-		{ label: 'Sign Pro', value: 'sign_pro' },
-		{ label: 'Get Sponsorship', value: 'get_sponsorship' },
-		{ label: 'Find Venue', value: 'find_venue' }
+		{ label: 'Sign Pro', value: 'sign_pro', description: 'Description for Sign Pro' },
+		{
+			label: 'Get Sponsorship',
+			value: 'get_sponsorship',
+			description: 'Description for Get Sponsorship'
+		},
+		{ label: 'Find Venue', value: 'find_venue', description: 'Description for Find Venue' }
 	];
 
 	$: selectedCampaignTypeLabel =
@@ -33,6 +37,7 @@
 	async function loadProfessionals() {
 		let { data, error } = await supabase.from('professional').select('*');
 		if (error) {
+			target;
 			console.error('Error loading professionals:', error);
 		} else {
 			const pros = data;
@@ -42,17 +47,19 @@
 	loadProfessionals();
 
 	function closeOverlay() {
-		setTimeout(() => {
-			overlayVisible = false;
-		}, 50);
+		overlayVisible = false;
+		document.body.style.overflow = 'auto'; // restore scrolling, if it was disabled
+		// Any other style resets you might need
+		location.reload();
 	}
 
-
 	function handleOutsideClick(event) {
-		console.log("Clicked outside:", event.target);
+		console.log('Clicked on:', event.target);
+		console.log('Current step:', currentStep);
+
 		if (event.target === event.currentTarget) {
-			console.log("Condition met");
-			closeOverlay();  // Assuming this function closes the overlay
+			console.log('Condition met for step', currentStep);
+			closeOverlay();
 		}
 	}
 
@@ -80,62 +87,65 @@
 	}
 </script>
 
-<div
-	class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50"
-	on:click={handleOutsideClick}
-	transition:fade={{ duration: 200 }}
->
-	<div class="relative bg-white p-4 rounded-lg max-w-xl w-full">
-		<!-- Close button -->
-		<button
-			class="absolute top-0 right-0 m-2 text-gray-700 hover:text-gray-900"
-			type="button"
-			on:click={closeOverlay}
-		>
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke="currentColor"
-				class="h-6 w-6"
+{#if overlayVisible}
+	<div
+		class={`fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 ${
+			overlayVisible ? '' : 'hidden'
+		}`}
+		on:click={handleOutsideClick}
+		on:keydown={handleOutsideClick}
+		transition:fade={{ duration: 200 }}
+	>
+		<div class="relative bg-white p-4 rounded-lg max-w-xl w-full">
+			<!-- Close button -->
+			<button
+				class="absolute top-0 right-0 m-2 text-gray-700 hover:text-gray-900"
+				type="button"
+				on:click|stopPropagation={closeOverlay}
 			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M6 18L18 6M6 6l12 12"
-				/>
-			</svg>
-		</button>
-
-		<form class="modal-form" on:submit|preventDefault={handleSubmit}>
-			{#if currentStep === 1}
-				<!-- Step 1: Choose Campaign Type -->
-				<!-- Campaign Type Selection -->
-				<fieldset>
-					<legend class="text-lg font-semibold text-black mb-2">Select Campaign Type:</legend>
-					<label class="block mb-2 text-black">
-						<input type="radio" bind:group={campaign.campaign_type} value="sign_pro" />
-						Sign Pro
-					</label>
-					<label class="block mb-2 text-black">
-						<input type="radio" bind:group={campaign.campaign_type} value="money" />
-						Money
-					</label>
-					<label class="block mb-2 text-black">
-						<input type="radio" bind:group={campaign.campaign_type} value="time" />
-						Time
-					</label>
-					<label class="block mb-2 text-black">
-						<input type="radio" bind:group={campaign.campaign_type} value="use of space" />
-						Use of Space
-					</label>
-				</fieldset>
-				<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600 text-white"
-					>Next</button
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					class="h-6 w-6"
 				>
-			{:else if currentStep === 2}
-				<div on:click={handleOutsideClick} class="outside-click-container">
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+
+			<form class="modal-form" on:submit|preventDefault={handleSubmit}>
+				{#if currentStep === 1}
+					<!-- Step 1: Choose Campaign Type -->
+					<!-- Campaign Type Selection -->
+					<fieldset>
+						<legend class="text-lg font-semibold text-black mb-2">Select Campaign Type:</legend>
+						<label class="block mb-2 text-black">
+							<input type="radio" bind:group={campaign.campaign_type} value="sign_pro" />
+							Sign Pro
+						</label>
+						<label class="block mb-2 text-black">
+							<input type="radio" bind:group={campaign.campaign_type} value="money" />
+							Money
+						</label>
+						<label class="block mb-2 text-black">
+							<input type="radio" bind:group={campaign.campaign_type} value="time" />
+							Time
+						</label>
+						<label class="block mb-2 text-black">
+							<input type="radio" bind:group={campaign.campaign_type} value="use of space" />
+							Use of Space
+						</label>
+					</fieldset>
+					<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600 text-white"
+						>Next</button
+					>
+				{:else if currentStep === 2}
 					<h2 class="text-xl font-bold mb-4 text-black">{selectedCampaignTypeLabel}</h2>
 
 					{#if campaign.campaign_type === 'sign_pro'}
@@ -145,28 +155,29 @@
 					{:else if campaign.campaign_type === 'find_venue'}
 						<FindVenue bind:campaign />
 					{/if}
-				</div>
 
-				<button on:click|preventDefault={prevStep} class="btn mt-4 bg-gray-600">Back</button>
-				<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600">Next</button>
-			{:else if currentStep === 3}
-				<!-- Step 3: Campaign Specific Details or other details -->
-				<!--... Whatever else you want to display for step 3 ...-->
+					<button on:click|preventDefault={prevStep} class="btn mt-4 bg-gray-600">Back</button>
+					<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600">Next</button>
+				{:else if currentStep === 3}
+					<!-- Step 3: Campaign Specific Details or other details -->
+					<!--... Whatever else you want to display for step 3 ...-->
 
-				<button on:click|preventDefault={prevStep} class="btn mt-4 bg-gray-600">Back</button>
-				<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600">Next</button>
-			{:else if currentStep === 4}
-				<!-- Step 4: Review & Submit -->
-				<h2 class="text-xl font-bold mb-4 text-black">{selectedCampaignTypeLabel}</h2>
-				<!--... Display a summary of the campaign data ...-->
-				<!--... Rest of the buttons ...-->
-			{/if}
-		</form>
+					<button on:click|preventDefault={prevStep} class="btn mt-4 bg-gray-600">Back</button>
+					<button on:click|preventDefault={nextStep} class="btn mt-4 bg-blue-600">Next</button>
+				{:else if currentStep === 4}
+					<!-- Step 4: Review & Submit -->
+					<h2 class="text-xl font-bold mb-4 text-black">{selectedCampaignTypeLabel}</h2>
+					<!--... Display a summary of the campaign data ...-->
+					<!--... Rest of the buttons ...-->
+				{/if}
+			</form>
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
-.visible {
-    display: block;
-}
+	.hidden {
+		visibility: hidden;
+		pointer-events: none;
+	}
 </style>
