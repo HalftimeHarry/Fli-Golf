61ed1002b5326c6ae407969d238c8b4046d889cb
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-08-21 18:40:14 +0000

    Use dollar

--- app/src/routes/rank/+layout.svelte
+++ app/src/routes/rank/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import AdminNavigation from '../lib/AdminNavigation.svelte';
+	import AdminNavigation from '$lib/AdminNavigation.svelte';
 	import { goto } from '$app/navigation';
 
 	let currentTile: number = 0;
