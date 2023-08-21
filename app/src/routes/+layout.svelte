1f460b5eaa047853cb06e953d762067eadec08bd
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-08-21 18:43:18 +0000

    Use more dollars

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
-	import authStore from '../lib/AuthStore';
+	import authStore from '$lib/AuthStore';
 	import { onMount } from 'svelte';
 	import type { AuthSession } from '@supabase/supabase-js';
 	import { supabase } from '../supabaseClient';
-	import Banner from '../lib/Banner.svelte';
+	import Banner from '$lib/Banner.svelte';
 
 	let session: AuthSession | null = null;
 	let role: 'Admin' | 'Participant' | null = null;
