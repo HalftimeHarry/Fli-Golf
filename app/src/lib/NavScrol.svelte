5fcad77aede08c334ddc625355ea666a28220859
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-08-25 14:53:43 +0000

    New scroll

--- app/src/lib/NavScrol.svelte
+++ app/src/lib/NavScrol.svelte
@@ -1,25 +1,17 @@
 <script>
+	let continuousText = '... Placeholder text for the ticker...'; // example text
+	const ScrollSpeed = 100;
+	const ScrollChars = 1;
+
+	function scrollMarquee() {
+		continuousText =
+			continuousText.substring(ScrollChars) + continuousText.substring(0, ScrollChars);
+		setTimeout(scrollMarquee, ScrollSpeed);
+	}
+
 	import { onMount } from 'svelte';
-
-	let fixedEnd;
-	let scrollingList;
-	let fixedStart;
-
 	onMount(() => {
-		const fixedStartRight = fixedStart.getBoundingClientRect().left;
-		const fixedEndWidth = fixedEnd.getBoundingClientRect().width;
-		const scrollingWidth = scrollingList.scrollWidth;
-
-		// Calculate the total distance the scrolling list should travel.
-		const distanceToTravel = fixedStartRight + scrollingWidth - fixedEndWidth;
-
-		// Set the distance and initial translation as CSS variables on the scrolling list.
-		scrollingList.style.setProperty('--distance', `${distanceToTravel}px`);
-		scrollingList.style.setProperty('--fixedEndWidth', `${fixedEndWidth}px`);
-
-		// Adjust animation duration based on distance.
-		const duration = 10 * (distanceToTravel / scrollingWidth);
-		scrollingList.style.animationDuration = `${duration}s`;
+		scrollMarquee();
 	});
 </script>
 
@@ -38,68 +30,31 @@
 		<a href="#" class="hover:underline">Team Picker</a>
 		<a href="#" class="hover:underline">Teams</a>
 	</div>
-
-	<div class="relative flex items-center justify-between border-t border-b border-white">
-		<div class="flex items-center">
-			<div class="fixed-end bg-green-500 text-white text-center py-2 flex-shrink-0 mr-4 px-4">
-				Next Tournament
+	<div class="container mx-auto w-full p-4 border-t border-white">
+		<div class="flex w-full">
+			<div class="flex-none w-64 h-14 bg-green-500 relative z-10">Next Tournament</div>
+			<div class="flex-grow ticker-wrapper relative overflow-hidden">
+				<input readonly bind:value={continuousText} class="w-full border-none bg-transparent" />
 			</div>
-			<div class="scrolling-list flex space-x-4">
-				<span>CHICAGO — RICH HARVEST FARMS — SEP 22—24</span>
-				<span>CHICAGO — RICH HARVEST FARMS — SEP 22—24</span>
-				<span>CHICAGO — RICH HARVEST FARMS — SEP 22—24</span>
+			<div class="flex-initial w-auto relative z-10 flex items-center justify-start space-x-4">
+				<button class="w-auto py-2 px-4 bg-blue-500 text-white hover:bg-blue-600">
+					MORE INFO <i class="fas fa-arrow-right text-white" />
+				</button>
+				<button class="w-auto py-2 px-4 bg-blue-500 text-white hover:bg-blue-600">
+					ATTEND EVENT <i class="fas fa-arrow-right text-white" />
+				</button>
 			</div>
-		</div>
-		<div class="fixed-start flex items-center border-l border-white">
-			<button class="py-2 px-4 bg-blue-500 text-white hover:bg-blue-600">
-				MORE INFO <i class="fas fa-arrow-right text-white" />
-			</button>
-			<button class="ml-0 py-2 px-4 bg-blue-500 text-white hover:bg-blue-600">
-				ATTEND EVENT <i class="fas fa-arrow-right text-white" />
-			</button>
 		</div>
 	</div>
 </div>
 
 <style>
-	@keyframes scroll {
-		0% {
-			transform: translateX(100%);
-		}
-		100% {
-			transform: translateX(calc(var(--distance) * -1));
-		}
-	}
-
-	.relative {
-		display: flex;
-		align-items: center;
-		position: relative;
-		overflow: hidden; /* Ensure nothing overflows out of this container */
-	}
-
-	.fixed-end {
-		flex-shrink: 0;
-		z-index: 2;
+	/* Additional styles */
+	.ticker-wrapper {
 		position: relative;
 	}
-
-	.fixed-start {
-		flex-shrink: 0;
-		z-index: 2;
-		position: relative;
-		display: flex;
-		left: -10px; /* Adjust this value to move the buttons more to the left */
-	}
-
-	.scrolling-list {
-		/* The animation definition here will be overridden by JavaScript */
-		animation: scroll 20s linear infinite;
-		display: flex; /* Flexbox display */
-		flex-wrap: nowrap; /* Prevent wrapping of the children */
-	}
-
-	.scrolling-list span {
-		white-space: nowrap; /* Prevent breaking within a span */
+	input {
+		font-family: inherit; /* Make input text look like regular text */
+		font-size: inherit; /* Make input text look like regular text */
 	}
 </style>
