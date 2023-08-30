66397cfacead80fb5172bf2bd4b72d758a0b58ae
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-08-30 01:23:00 +0000

    New config

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -1,18 +1,10 @@
-import vercelAdapter from '@sveltejs/adapter-vercel';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
-
-    kit: {
-        adapter: vercelAdapter({
-            // specify the runtime version
-            runtime: 'nodejs16.x' // or 'nodejs18.x' if you prefer
-        }),
-
-        // other kit options...
-    }
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
