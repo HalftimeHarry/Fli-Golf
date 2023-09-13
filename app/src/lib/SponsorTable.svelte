85bece04a88acd0b1e8d26b14189229f099e93de
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-13 22:08:19 +0000

    Team logos working

--- app/src/lib/SponsorTable.svelte
+++ app/src/lib/SponsorTable.svelte
@@ -1,81 +1,73 @@
 <script lang="ts">
-	import { supabase } from '../supabaseClient';
-	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
-	import type { TableSource } from '@skeletonlabs/skeleton';
-	import { onMount } from 'svelte';
+    import { supabase } from '../supabaseClient';
+    import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
+    import type { TableSource } from '@skeletonlabs/skeleton';
+    import { onMount } from 'svelte';
 
-	let tableSimple: TableSource = {
-		head: [],
-		body: [],
-		meta: [],
-		foot: []
-	};
+    let tableSimple: TableSource = {
+        head: [],
+        body: [],
+        meta: [],
+        foot: []
+    };
 
-	function mapDataToImageTable(data, columns) {
-		return data.map((item) => {
-			let row = {};
-			columns.forEach((col) => {
-				if (col === 'sponsor_logo_url') {
-					row[col] = `<img src="${item[col]}" alt="Sponsor Logo" width="50">`;
-				} else {
-					row[col] = item[col];
-				}
-			});
-			return row;
-		});
-	}
+    // Define the base URL and folder for your bucket
+    const BUCKET_BASE_URL = 'https://nzctebxobnjbvyygzpfq.supabase.co/storage/v1/object/public/';
+    const FOLDER = 'sponsor_logos/';
 
-	onMount(async () => {
-		// ... your existing fetch logic ...
+    onMount(async () => {
+        let { data: sponsors, error: sponsorsError } = await supabase
+            .from('sponsors')
+            .select('id, company_name, company_url, contact_person, contact_email, created_at, is_sponsoring, sponsor_logo_url, level');
+        
+        if (sponsorsError) {
+            console.error('Error fetching sponsors:', sponsorsError);
+        } else {
+            // Concatenate the bucket base URL, folder, and the logo URL
+            sponsors = sponsors.map((sponsor) => ({
+                ...sponsor,
+                sponsor_logo_url: `<img src="${BUCKET_BASE_URL}${FOLDER}${sponsor.sponsor_logo_url}" alt="Sponsor Logo" width="50">`
+            }));
 
-		let { data, error } = await supabase
-			.from('sponsors')
-			.select(
-				'id, company_name, company_url, contact_person, contact_email, created_at, is_sponsoring, sponsor_logo_url, level'
-			);
-		if (error) {
-			console.error('Error fetching sponsors:', error);
-		} else {
-			// Assuming data is not null
-			tableSimple = {
-				head: [
-					'Company Name',
-					'Company URL',
-					'Contact Person',
-					'Contact Email',
-					'Created At',
-					'Is Sponsoring',
-					'Sponsor Logo',
-					'Level'
-				],
-				body: mapDataToImageTable(data, [
-					'company_name',
-					'company_url',
-					'contact_person',
-					'contact_email',
-					'created_at',
-					'is_sponsoring',
-					'sponsor_logo_url',
-					'level'
-				]),
-				meta: mapDataToImageTable(data, [
-					'id',
-					'company_name',
-					'company_url',
-					'contact_person',
-					'contact_email',
-					'created_at',
-					'is_sponsoring',
-					'sponsor_logo_url',
-					'level'
-				]),
-				foot: ['Total', '', '', '', '', '', '', `${data.length}`]
-			};
-		}
-	});
+            tableSimple = {
+                head: [
+                    'Company Name',
+                    'Company URL',
+                    'Contact Person',
+                    'Contact Email',
+                    'Created At',
+                    'Is Sponsoring',
+                    'Sponsor Logo',
+                    'Level'
+                ],
+                body: tableMapperValues(sponsors, [
+                    'company_name',
+                    'company_url',
+                    'contact_person',
+                    'contact_email',
+                    'created_at',
+                    'is_sponsoring',
+                    'sponsor_logo_url',
+                    'level'
+                ]),
+                meta: tableMapperValues(sponsors, [
+                    'id',
+                    'company_name',
+                    'company_url',
+                    'contact_person',
+                    'contact_email',
+                    'created_at',
+                    'is_sponsoring',
+                    'sponsor_logo_url',
+                    'level'
+                ]),
+                foot: ['Total', '', '', '', '', '', '', `${sponsors.length}`]
+            };
+        }
+    });
 </script>
 
 <div class="container mx-auto px-4">
-	<h1 class="text-4xl font-bold mb-4 text-white">Sponsors</h1>
-	<Table source={tableSimple} />
+    <h1 class="text-4xl font-bold mb-4 text-white">Sponsors</h1>
+    <Table source={tableSimple} />
 </div>
