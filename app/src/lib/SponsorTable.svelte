e4c25e1ff4b8e6be6a80936e081e8546c96f2459
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-13 19:11:25 +0000

    Fixed code to deploy

--- app/src/lib/SponsorTable.svelte
+++ app/src/lib/SponsorTable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { supabase } from '/workspace/Fli-Golf/app/src/supabaseClient';
+	import { supabase } from '../supabaseClient';
 	import { Table, tableMapperValues } from '@skeletonlabs/skeleton';
 	import type { TableSource } from '@skeletonlabs/skeleton';
 	import { onMount } from 'svelte';
@@ -12,9 +12,9 @@
 	};
 
 	function mapDataToImageTable(data, columns) {
-		return data.map(item => {
+		return data.map((item) => {
 			let row = {};
-			columns.forEach(col => {
+			columns.forEach((col) => {
 				if (col === 'sponsor_logo_url') {
 					row[col] = `<img src="${item[col]}" alt="Sponsor Logo" width="50">`;
 				} else {
@@ -25,47 +25,57 @@
 		});
 	}
 
-onMount(async () => {
-    // ... your existing fetch logic ...
+	onMount(async () => {
+		// ... your existing fetch logic ...
 
-    let { data, error } = await supabase
-        .from('sponsors')
-        .select('id, company_name, company_url, contact_person, contact_email, created_at, is_sponsoring, sponsor_logo_url, level');
-    if (error) {
-        console.error('Error fetching sponsors:', error);
-    } else {
-        // Assuming data is not null
-        tableSimple = {
-            head: ['Company Name', 'Company URL', 'Contact Person', 'Contact Email', 'Created At', 'Is Sponsoring', 'Sponsor Logo', 'Level'],
-            body: mapDataToImageTable(data, [
-                'company_name',
-                'company_url',
-                'contact_person',
-                'contact_email',
-                'created_at',
-                'is_sponsoring',
-                'sponsor_logo_url',
-                'level'
-            ]),
-            meta: mapDataToImageTable(data, [
-                'id',
-                'company_name',
-                'company_url',
-                'contact_person',
-                'contact_email',
-                'created_at',
-                'is_sponsoring',
-                'sponsor_logo_url',
-                'level'
-            ]),
-            foot: ['Total', '', '', '', '', '', '', `${data.length}`]
-        };
-    }
-});
-
+		let { data, error } = await supabase
+			.from('sponsors')
+			.select(
+				'id, company_name, company_url, contact_person, contact_email, created_at, is_sponsoring, sponsor_logo_url, level'
+			);
+		if (error) {
+			console.error('Error fetching sponsors:', error);
+		} else {
+			// Assuming data is not null
+			tableSimple = {
+				head: [
+					'Company Name',
+					'Company URL',
+					'Contact Person',
+					'Contact Email',
+					'Created At',
+					'Is Sponsoring',
+					'Sponsor Logo',
+					'Level'
+				],
+				body: mapDataToImageTable(data, [
+					'company_name',
+					'company_url',
+					'contact_person',
+					'contact_email',
+					'created_at',
+					'is_sponsoring',
+					'sponsor_logo_url',
+					'level'
+				]),
+				meta: mapDataToImageTable(data, [
+					'id',
+					'company_name',
+					'company_url',
+					'contact_person',
+					'contact_email',
+					'created_at',
+					'is_sponsoring',
+					'sponsor_logo_url',
+					'level'
+				]),
+				foot: ['Total', '', '', '', '', '', '', `${data.length}`]
+			};
+		}
+	});
 </script>
 
 <div class="container mx-auto px-4">
-    <h1 class="text-4xl font-bold mb-4 text-white">Sponsors</h1>
-    <Table source={tableSimple} />
+	<h1 class="text-4xl font-bold mb-4 text-white">Sponsors</h1>
+	<Table source={tableSimple} />
 </div>
