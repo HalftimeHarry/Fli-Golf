ed7cd032fcbb9f6a9642ee8eedccbec3fd015173
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-17 22:02:27 +0000

    Do FAQ Next

--- app/src/lib/NavScrol.svelte
+++ app/src/lib/NavScrol.svelte
@@ -1,11 +1,17 @@
 <script>
 	import VegasPopUp from './VegasPopUp.svelte';
 	import { onMount } from 'svelte';
-	import { overlayStore } from '../lib/overlayStore'; // Assuming this is the path to your store
+	import { overlayStore } from '../lib/overlayStore';
 
-	let continuousText = '... Placeholder text for the ticker...'; // example text
+	let continuousText = '... Placeholder text for the ticker...';
 	const ScrollSpeed = 100;
 	const ScrollChars = 1;
+
+	let menuOpen = false;
+
+	function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
 
 	function scrollMarquee() {
 		continuousText =
@@ -18,7 +24,6 @@
 	});
 
 	function toggleVegasPopUp() {
-		// If the VegasPopUp is already active, close it. Else, set it as the active overlay.
 		if ($overlayStore === 'vegas') {
 			overlayStore.set('');
 		} else {
@@ -27,29 +32,36 @@
 	}
 
 	function closeVegasPopUp() {
-		overlayStore.set(''); // Close any overlay
+		overlayStore.set('');
 	}
 </script>
 
 <div class="container mx-auto p-4 border-t border-white z-10">
-	<div class="top-row flex flex-row space-x-4 mb-8 justify-between">
-		<a href="schedule" class="hover:underline">Schedule</a>
-		<a href="players" class="hover:underline">Players</a>
-		<a href="teams" class="hover:underline">Teams</a>
-		<a href="sponsors" class="hover:underline">Sponsors</a>
-		<a href="venues" class="hover:underline">Venues</a>
-		<a href="#" class="hover:underline">Standings</a>
-		<a href="#" class="hover:underline">Fantasy</a>
-		<a href="#" class="hover:underline">Statistics</a>
-		<a href="#" class="hover:underline">Leaderboard</a>
-		<a href="#" class="hover:underline">Where to Watch</a>
-		<a href="info" class="hover:underline">Info</a>
-		<button
-			class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
-			on:click={toggleVegasPopUp}
-		>
-			Vegas
-		</button>
+	<div class="top-row flex flex-row space-x-4 mb-8 justify-between md:justify-start">
+		<!-- Hamburger Menu (visible on small screens) -->
+		<button class="md:hidden p-2 mr-4" on:click={toggleMenu}> ☰ </button>
+
+		<!-- Main Menu Links -->
+		<div class={`space-x-4 ${menuOpen ? 'block' : 'hidden'} md:flex`}>
+			<a href="schedule" class="hover:underline">Schedule</a>
+			<a href="players" class="hover:underline">Players</a>
+			<a href="teams" class="hover:underline">Teams</a>
+			<a href="sponsors" class="hover:underline">Sponsors</a>
+			<a href="venues" class="hover:underline">Venues</a>
+			<a href="#" class="hover:underline">Standings</a>
+			<a href="#" class="hover:underline">Fantasy</a>
+			<a href="#" class="hover:underline">Statistics</a>
+			<a href="#" class="hover:underline">Leaderboard</a>
+			<a href="#" class="hover:underline">Where to Watch</a>
+			<a href="info" class="hover:underline">Info</a>
+			<a href="questions" class="hover:underline">FAQ</a>
+			<button
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded focus:outline-none focus:shadow-outline"
+				on:click={toggleVegasPopUp}
+			>
+				Vegas
+			</button>
+		</div>
 		{#if $overlayStore === 'vegas'}
 			<VegasPopUp on:close={closeVegasPopUp} />
 		{/if}
