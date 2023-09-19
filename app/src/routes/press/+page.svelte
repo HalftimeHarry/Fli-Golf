f90319252cf23f00c74ef942f480ee156ef02f11
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-19 14:50:31 +0000

    Added Press

---
+++ app/src/routes/press/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+	// Any component-specific logic can go here
+</script>
+
+<div class="container mx-auto p-6 bg-gray-900 text-white">
+	<!-- Group Image of Founders -->
+	<img src="group.jpg" alt="Founders of FLI GOLF TOUR" class="w-full max-w-2xl mx-auto" />
+	<p class="text-center mt-4">From left to right: Corey La Russo, Dustin Dinsmore, Tony Smart, and Andrew Panza</p>
+
+	<!-- PRESS RELEASE -->
+	<div class="mt-8">
+		<h1 class="text-2xl font-bold text-center mb-4">PRESS RELEASE</h1>
+		<p class="text-center mb-2">Date: 11/1/2023</p>
+		<p class="text-center mb-4">Location: Las Vegas</p>
+		
+		<p class="mb-4">Today, the world of disc golf takes an unprecedented leap into the future with the introduction of the FLI GOLF TOUR - Pairs Edition. This pioneering tour embarks on a journey across college campuses in America, uniting the world's top disc golfers in electrifying male-female pair competitions, underpinning the spirit of gender equality in sports.</p>
+
+		<h2 class="text-xl font-semibold mb-2">Event Highlights:</h2>
+		<ul class="list-disc pl-5 mb-4">
+			<li>Witness the thrill of two-person teams, as one male and one female disc golfer come together, demonstrating unmatched skill, strategy, and synergy.</li>
+			<li>An exhilarating series of 1-day tournaments awaits fans, with events unfolding monthly on diverse college campuses.</li>
+			<li>Each course promises to be a spectacle. Expertly crafted, they aim to challenge the sport's elites while delivering a visual feast for spectators.</li>
+		</ul>
+
+		<h2 class="text-xl font-semibold mb-2">Exclusive Tour Features:</h2>
+		<ul class="list-disc pl-5 mb-4">
+			<li>The tour stands as a beacon for top-tier talent, assembling 24 of the world's best disc golfers.</li>
+			<li>Setting new benchmarks, the FLI GOLF TOUR announces the most generous prize money in the sport's history.</li>
+			<li>The meticulously designed courses, combined with the energy of college campuses, ensure fans experience every heart-stopping moment up close.</li>
+		</ul>
+
+		<h2 class="text-xl font-semibold mb-2">Tour Vision:</h2>
+		<ul class="list-disc pl-5 mb-4">
+			<li>FLI GOLF TOUR is not just a competition; it's a movement to elevate disc golf to unprecedented heights.</li>
+			<li>The mixed-gender pairs format is a testament to the tour's commitment to promoting gender equality in the world of sports.</li>
+			<li>Merging sports with entertainment, the tour aims to provide a captivating experience for fans and attendees.</li>
+			<li>Beyond entertainment, the tour seeks to inspire the next generation, laying the foundation for the sport's vibrant future.</li>
+		</ul>
+
+		<p class="mb-4">In collaboration with trusted sportsbook partners, the FLI GOLF TOUR also introduces legal gambling, infusing an added layer of excitement and engagement for fans. As disc golf's horizon broadens, the FLI GOLF TOUR - Pairs Edition stands as a testament to innovation, excellence, and the spirit of inclusivity.</p>
+
+		<p class="mb-4">For more information, schedule details, and ticketing, please visit <span class="text-blue-500 underline">[Insert Website/Contact Information]</span>.</p>
+
+		<h2 class="text-xl font-semibold mb-2">About FLI GOLF TOUR</h2>
+		<p>[Insert brief about section – company history, previous accomplishments, mission statement, etc.]</p>
+
+		<h2 class="text-xl font-semibold mt-4 mb-2">Press Contact:</h2>
+		<p>Andrew Panza</p>
+		<p>andrew@fligolf.com</p>
+		<p>(716) 572-8319</p>
+	</div>
+</div>
