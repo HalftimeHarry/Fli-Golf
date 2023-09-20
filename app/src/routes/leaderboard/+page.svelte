f1409a52ce0b50970c21b9424cb2863124e2a9db
Dustin L Dinsmore <ddinsmore8@gmail.com>
2023-09-20 14:07:10 +0000

    Star Leaderboard

--- app/src/routes/leaderboard/+page.svelte
+++ app/src/routes/leaderboard/+page.svelte
@@ -6,9 +6,11 @@
 	let error;
 
 	onMount(async () => {
-		const { data: resultsData, error: resultsError } = await supabase
-			.from('tournament_results')
-			.select('*');
+		const { data: resultsData, error: resultsError } = await supabase.from('tournament_results')
+			.select(`
+        *, 
+        tournaments(tournament_name) 
+    `);
 
 		if (resultsError) {
 			error = resultsError;
@@ -29,12 +31,13 @@
 
 			const teamMap = {};
 			teamsData.forEach((team) => {
-				teamMap[team.id] = team.team_name;
+				teamMap[team.id] = team.team_name; // Use 'team_name' if that's the column name
 			});
 
 			tournament_results = resultsData.map((result) => ({
 				...result,
-				teamName: teamMap[result.team_id]
+				teamName: teamMap[result.team_id],
+				tournamentName: result.tournaments.tournament_name
 			}));
 		}
 	});
@@ -48,26 +51,32 @@
 	<table class="min-w-full divide-y divide-gray-200 text-black">
 		<thead>
 			<tr>
-				<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+				<th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
 					Position
 				</th>
-				<th class="px-6 py-4 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+				<th class="px-6 py-4 text-left text-xs font-medium text-white uppercase tracking-wider">
+					Tournament Name
+				</th>
+				<th class="px-6 py-4 text-left text-xs font-medium text-white uppercase tracking-wider">
 					Team Name
 				</th>
-				<th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+				<th class="px-6 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
 					Score
 				</th>
 			</tr>
 		</thead>
-		<tbody class="bg-white divide-y divide-gray-200">
+		<tbody class="bg-white divide-y divide-black">
 			{#each tournament_results as result (result.result_id)}
 				<tr>
 					<td class="px-6 py-4 whitespace-nowrap">
 						{result.position}
 					</td>
 					<td class="px-6 py-4 whitespace-nowrap">
+						{result.tournamentName}
+						<!-- Displaying the tournament name -->
+					</td>
+					<td class="px-6 py-4 whitespace-nowrap">
 						{result.teamName}
-						<!-- Note the change here -->
 					</td>
 					<td class="px-6 py-4 whitespace-nowrap">
 						{result.score}
